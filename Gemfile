ea3a3e1150082b4714d469b9adec5c06d06c9b41
James <james@ruby-code.com>
2018-02-16 14:27:46 -0500

    Make LB100 lights work; cleaned up more code; 0 rubocop errors.

--- Gemfile
+++ Gemfile
@@ -1,8 +1,12 @@
+# frozen_string_literal: true
+
 source 'https://rubygems.org'
 gem 'faraday', '~> 0.13'
 gem 'faraday_middleware'
 
 group :development do
-  gem 'rubocop'
   gem 'pry'
+  gem 'rspec'
+  gem 'rubocop'
+  gem 'webmock'
 end
--- lib/tp_link/api.rb
+++ lib/tp_link/api.rb
@@ -1,30 +1,40 @@
 # frozen_string_literal: true
+
+# TPLink Module
 module TPLink
-  # Communicate with the tplink cloud api
-  TOKEN_API = Faraday.new('https://wap.tplinkcloud.com') do |c|
-    # c.use FaradayMiddleware::ParseJson,       content_type: 'application/json'
-    # c.use Faraday::Response::Logger,          Logger.new('faraday.log')
-    # c.use FaradayMiddleware::FollowRedirects, limit: 31
-    # raise exceptions on 40x, 50x responses
-    # c.use Faraday::Response::RaiseError
-    # c.use Faraday::Adapter::NetHttp
-    c.request :json
-    c.response :json, :content_type => /\bjson$/
-    c.adapter Faraday.default_adapter
-    c.params['appName'] = "Kasa_Android"
-    c.params['termID']  = Proc.new { @config.terminal_uuid }
-    c.params['appVer'] = '1.4.4.607'
-    c.params['ospf'] = 'Android+6.0.1'
-    c.params['netType'] = 'wifi'
-    c.params['locale'] =  'es_ES'
-    c.headers['Connection'] = 'Keep-Alive'
-    c.headers['User-Agent'] = 'Dalvik/2.1.0 (Linux; U; Android 6.0.1; A0001 Build/M4B30X)'
-    c.headers['Content-Type'] = "applicatoin/json;charset=utf-8"
-    c.headers['Accept'] = "application/json, text/plain, */*"
-
-  end
-
+  # Talk to TPLink's API
   class API
+    DEFAULT_PARAMS = {
+      'appName' =>  'Kasa_Android',
+      'termID' =>  proc { @config.terminal_uuid },
+      'appVer' =>  '1.4.4.607',
+      'ospf' =>  'Android+6.0.1',
+      'netType' =>  'wifi',
+      'locale' =>  'es_ES'
+    }.freeze
+    DEFAULT_HEADERS = {
+      'Connection' => 'Keep-Alive',
+      'User-Agent' => 'Dalvik/2.1.0 (Linux; U; Android 6.0.1; ' \
+                                'A0001 Build/M4B30X)',
+      'Content-Type' => 'applicatoin/json;charset=utf-8',
+      'Accept' => 'application/json, text/plain, */*'
+    }.freeze
+    TOKEN_API = Faraday.new('https://wap.tplinkcloud.com') do |c|
+      c.request :json
+      c.response :json, content_type: /\bjson$/i
+      c.adapter Faraday.default_adapter
+      c.params.merge!(DEFAULT_PARAMS)
+      c.headers.merge!(DEFAULT_HEADERS)
+    end
+
+    DEVICE_API = Faraday.new('https://wap.tplinkcloud.com') do |c|
+      c.request :json
+      c.response :json, content_type: /\bjson$/i
+      c.adapter Faraday.default_adapter
+      c.params.merge!(DEFAULT_PARAMS)
+      c.headers.merge!(DEFAULT_HEADERS)
+    end
+
     DEFAULT_OPTIONS = {
       config: "#{ENV['HOME']}/.tp_link"
     }.freeze
@@ -33,23 +43,51 @@ module TPLink
       @config = Config.new(options[:config])
     end
 
-
-    def get_token
-      response=TOKEN_API.post do |req|
-        req.url '/'
-        req.body = {method:"login",url:"https://wap.tplinkcloud.com",params: @config.to_hash}.to_json
+    def token(regen = false)
+      return @token if @token && regen == false
+      response = TOKEN_API.post do |req|
+        req.body = { method: 'login', url: 'https://wap.tplinkcloud.com',
+                     params: @config.to_hash }.to_json
       end
-      parse_response(response)['token']
+      @token = parse_response(response)['token']
+    end
+
+    def device_list
+      response = DEVICE_API.post do |req|
+        req.params['token'] = token
+        req.body = { method: 'getDeviceList' }.to_json
+      end
+      @device_list = parse_response(response)['deviceList']
+    end
+
+    def send_data(device, data)
+      conn = data_connection(device)
+      conn.post do |req|
+        req.body = { method: 'passthrough',
+                     params: { deviceId: device.id,
+                               requestData: data.to_json } }.to_json
+      end
     end
 
     private
-    
-    def parse_response(res)
-      raise TPLinkCloudError.new("Generic TPLinkCloud Error") unless res.success?
-      response = JSON.parse(res.body)
-      raise TPLinkCloudError.new("TPLinkCloud API Error") unless res.body['error'].to_i == 0
-      return response['result']
+
+    def data_connection(device)
+      Faraday.new(device.url) do |c|
+        c.request :json
+        c.response :json, content_type: /\bjson$/i
+        c.adapter Faraday.default_adapter
+        # c.params.merge!(DEFAULT_PARAMS)
+        c.params['token'] = token
+        c.headers.merge!(DEFAULT_HEADERS)
+      end
     end
 
+    def parse_response(res)
+      raise TPLinkCloudError, 'Generic TPLinkCloud Error' unless res.success?
+      response = JSON.parse(res.body)
+      raise TPLinkCloudError, 'TPLinkCloud API Error' \
+        unless res.body['error'].to_i.zero?
+      response['result']
+    end
   end
 end
